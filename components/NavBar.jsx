b92eaa3c61335c40ea0e60f4ee71e90ed0ba5c38
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2024-02-05 14:14:23 +0300

    project setup with next

---
+++ components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function NavBar() {
+	return (
+		<div className="w-full bg-[#333333] bg-opacity-20 p-5 border border-gray-400 border-l-0 border-t-0 border-r-0">
+			<h1 className="text-black text-2xl font-medium">Country Search</h1>
+		</div>
+	)
+}
+
+export default NavBar
