7f0d48a0832d455bf79a2648a4c1bdcc32a89781
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2024-02-05 16:13:07 +0300

    auto select implemented for after query and init load

--- components/CountryList.jsx
+++ components/CountryList.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 import { ApolloClient, InMemoryCache, gql, useQuery } from "@apollo/client"
 
@@ -29,6 +29,14 @@ function CountryList({ search = "" }) {
 		{ client }
 	)
 
+	useEffect(() => {
+		if (!loading && data?.countries.length > 10)
+			setSelected(data?.countries[9]?.code || "")
+		else {
+			setSelected(data?.countries[data.countries.length - 1]?.code || "")
+		}
+	}, [loading, data])
+
 	if (loading || error) {
 		return (
 			<div className="flex-1 mt-12">
@@ -39,7 +47,7 @@ function CountryList({ search = "" }) {
 
 	return (
 		<div className="w-full flex-1 flex flex-col flex-wrap items-center gap-4 mt-2">
-			{data.countries.map((country) => (
+			{data.countries.map((country, index) => (
 				<CountryComp
 					key={country.code}
 					country={country}
